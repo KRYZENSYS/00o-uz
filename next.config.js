c27c7313df2bf0554416e4ac34fe0f7fd9822cbc
KRYZENSYS <f91186645@gmail.com>
2026-07-15 00:03:10 +0500

    fixed next config

--- next.config.js
+++ next.config.js
@@ -2,17 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { unoptimized: true },
-  async redirects() {
-    return [
-      { source: '/', destination: '/uz', permanent: false },
-      { source: '/en', destination: '/en/home', permanent: false },
-      { source: '/ru', destination: '/ru/home', permanent: false },
-    ];
-  },
   async rewrites() {
     return [
-      { source: '/uz', destination: '/uz/home' },
-      { source: '/ru', destination: '/ru/home' },
+      { source: '/', destination: '/uz/home' },
     ];
   },
 };
