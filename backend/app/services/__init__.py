080b9710f0a1a016736a552a4d98b1587a5fa201
KRYZENSYS <f91186645@gmail.com>
2026-08-11 15:34:47 +0500

    chore: export new services

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,1 +1,24 @@
-"""Services init"""
+"""Services init."""
+from app.services import (
+    ip_service,
+    dns_service,
+    whois_service,
+    translate_service,
+    weather_service,
+    news_service,
+    currency_service,
+    github_service,
+    download_service,
+)
+
+__all__ = [
+    "ip_service",
+    "dns_service",
+    "whois_service",
+    "translate_service",
+    "weather_service",
+    "news_service",
+    "currency_service",
+    "github_service",
+    "download_service",
+]
